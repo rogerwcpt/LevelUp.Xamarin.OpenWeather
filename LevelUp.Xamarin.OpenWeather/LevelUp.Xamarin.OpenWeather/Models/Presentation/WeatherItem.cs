34c364659d77cfcaf5455c72234b289cb8df0cad
rogerweiss <roger.d.weiss@gmail.com>
2017-05-03 19:00:36 +0200

    Cleanup Added Color Plugin

--- LevelUp.Xamarin.OpenWeather/LevelUp.Xamarin.OpenWeather/Models/Presentation/WeatherItem.cs
+++ LevelUp.Xamarin.OpenWeather/LevelUp.Xamarin.OpenWeather/Models/Presentation/WeatherItem.cs
@@ -3,13 +3,15 @@ namespace LevelUp.Xamarin.OpenWeather.Models.Presentation
 {
 	public class WeatherItem
 	{
-		public WeatherItem(string title, string value)
+		public WeatherItem(string title, string value, bool oddColor = false)
 		{
 			Title = title;
 			Value = value;
+			OddColor = oddColor;
 		}
 
 		public string Title { get; }
 		public string Value { get; }
+		public bool OddColor { get; }
 	}
 }
