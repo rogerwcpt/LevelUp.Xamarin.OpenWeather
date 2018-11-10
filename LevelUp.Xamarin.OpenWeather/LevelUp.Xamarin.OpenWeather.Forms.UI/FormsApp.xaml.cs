2cec0d61921739d12531caf69d711b86f57353d0
Roger Weiss <roger@cowboyaliens.com>
2018-11-10 16:59:34 +0200

    Initial commit of Forms Project and Droid Forms Project

---
+++ LevelUp.Xamarin.OpenWeather/LevelUp.Xamarin.OpenWeather.Forms.UI/FormsApp.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace LevelUp.Xamarin.OpenWeather.Forms.UI
+{
+    public partial class FormsApp : Application
+    {
+        public FormsApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
