faeb3fe3e3f2c438a137963adeb1ee662f2dbbd3
Roger Weiss <roger.d.weiss@gmail.com>
2019-01-02 19:28:18 +0200

    Added XamlCompilation option for Xaml code behind

--- LevelUp.Xamarin.OpenWeather/LevelUp.Xamarin.OpenWeather.Forms.UI/Views/MainView.xaml.cs
+++ LevelUp.Xamarin.OpenWeather/LevelUp.Xamarin.OpenWeather.Forms.UI/Views/MainView.xaml.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 using MvvmCross.Forms.Views;
 using LevelUp.Xamarin.OpenWeather.ViewModels;
 using MvvmCross.Forms.Presenters.Attributes;
 
 namespace LevelUp.Xamarin.OpenWeather.Forms.UI.Views
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainView : MvxContentPage<MainViewModel>
     {
         public MainView()
